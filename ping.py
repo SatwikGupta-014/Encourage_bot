c8ca3906ef12716c6cb817a5e79e6d111aa65fec
Satwik Gupta <satwik.gupta014@gmail.com>
2025-09-28 05:05:23 +0530

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('/')
 
 @app.route('/')
 def home():
-  return "Hello. I am alive!"
+    return "Hello. I am alive!"
 
 def run():
-  app.run(host = '0.0.0.0', port = 8080)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-  t = Thread(target = run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
